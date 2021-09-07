b17940e47cd80d66e6f9d88816d954f85d03689e
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-09-07 16:56:57 +0900

    title, statusbar added referring theme provider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 import styled, {ThemeProvider} from "styled-components/native";
 import {theme} from "./theme";
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
   flex: 1;
-  background-color: #fff;
+  background-color: ${({theme}) => theme.background};
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
 `
 
+const Title = styled.Text`
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({theme}) => theme.main};
+  width: 100%;
+  align-items: flex-end;
+  padding: 0 20px;
+`
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
             <Container>
-                <Text>ToDo</Text>
-                <StatusBar style="auto" />
+                <Title>To Do List</Title>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor={theme.background}
+                />
             </Container>
         </ThemeProvider>
     );
